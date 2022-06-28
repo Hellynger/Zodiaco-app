fae89d1b99335e645872104a319db306538cf745
Hellynger St Rose <you@example.com>
2022-06-28 00:07:44 -0500

    cambio en el login: si se ingresa mal el usuario puede volver a llenar el formulario

---
+++ resources/views/idioma/PedirIdioma.php
@@ -0,0 +1,93 @@
+<?
+include('../includes/header.php');
+encabezado();
+?>
+
+<?php
+function idiomas()
+{
+
+    ?>
+
+    <?php
+
+
+    function alerta_usuario($message)
+    {
+        echo "<script>alert('$message');</script>";
+    }
+
+    if (isset($_POST['enviar'])) {
+        if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+            echo "Debe llenar todos los campos";
+            
+        }
+        elseif ($_POST['usuario'] == "test" and $_POST['clave'] == "test") {
+            session_start();
+            $_SESSION['usuario'] = $_POST['usuario'];
+            $_SESSION['clave'] = $_POST['clave'];
+            alerta_usuario("Has iniciado sesión como " . $_SESSION['usuario']);
+
+
+        }
+        elseif ($_POST['usuario'] == "helly" and $_POST['clave'] == "4321") {
+            session_start();
+            $_SESSION['usuario'] = $_POST['usuario'];
+            $_SESSION['clave'] = $_POST['clave'];
+            alerta_usuario("Has iniciado sesión como " . $_SESSION['usuario']);
+
+        }
+        elseif ($_POST['usuario'] == "fercu" and $_POST['clave'] == "1234") {
+            session_start();
+            $_SESSION['usuario'] = $_POST['usuario'];
+            $_SESSION['clave'] = $_POST['clave'];
+            alerta_usuario("Has iniciado sesión como " . $_SESSION['usuario']);
+        }else{
+            
+            
+            alerta_usuario("Error en los datos");
+            header("Location:  ../../views/inicioSesion/inicio-sesion.php");
+        }
+
+    }
+    ?>
+
+    <head>
+        <title>Pedir el idioma del usuario</title>
+
+
+    </head>
+    <body data-dark>
+    <section class="section">
+
+
+        <h2 class="title-form" data-dark>Escoge tu idioma de preferencia</h2>
+        <div class="banderas">
+
+            <a href="javascript:enviarIdioma('es');"><img src="../../imgs/españa.png" alt="españa"
+                                                          style="width: 130px; height: 130px;"></a>
+
+            <a href="javascript:enviarIdioma('en');"><img src="../../imgs/ingles.png" alt="ingles"
+                                                          style="width: 150px; height: 150px;"></a>
+
+            <a href="javascript:enviarIdioma('fr');"><img src="../../imgs/francia.png" alt="francia"
+                                                          style="width: 150px; height: 140px;"></a>
+
+        </div>
+
+
+    </section>
+
+    </body>
+    <script language="javascript" type="text/javascript">
+        function enviarIdioma(idioma) {
+            location.href = "../../../app/Controllers/Cookie/grabarCookie.php?idiomaUsuario=" + idioma;
+        }
+    </script>
+    <script src="../../js/index.js" type="module"></script>
+
+    <?php
+} ?>
+
+
+ 
